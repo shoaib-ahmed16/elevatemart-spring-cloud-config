952039cfa2035adfe746b41f96e70c76b69ed04b
shoaib-ahmed16 <shoaib-ahmed16>
2024-03-20 14:31:43 +0530

    Adding the changes

---
+++ src/main/java/com/elevatemart/elevatemartspringcloudconfig/ElevatemartSpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.elevatemart.elevatemartspringcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatemartSpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatemartSpringCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elevatemart/elevatemartspringcloudconfig/ElevatemartSpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevatemart.elevatemartspringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatemartSpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
